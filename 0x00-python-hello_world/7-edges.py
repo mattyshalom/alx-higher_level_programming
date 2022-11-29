e6202117c91fdd12b18e340f00055658bd27b7af
mattyshalom <olusegesunday@yahoo.com>
2022-11-28 18:15:40 -0800

    Copy - Cut - Paste from a word

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+First_3_letter = word[:3]
+Last_2_letter = word[-2:]
+Middle_word = word[1:-1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
