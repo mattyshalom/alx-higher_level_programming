08f3dd83d62b2ab8823c5dc9224f0d8d07e70c9d
mattyshalom <olusegesunday@yahoo.com>
2022-12-04 02:35:07 -0800

    a function that prints a matrix of integers.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    sum1 = 0
-    sum2 = 0
-    t1 = tuple_a + (0, 0)
-    t2 = tuple_b + (0, 0)
-
-    sum1 += t1[0] + t2[0]
-    sum2 += t1[1] + t2[1]
-
-    return (sum1, sum2)
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
